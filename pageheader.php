61522b8aa21c9321f46423b2d96a78489c000863
caipi <caipi@ppbw.be>
2012-06-04 09:38:14 +0300

    Ajout d'une "news" pour réunion ce vendredi

--- pageheader.php
+++ pageheader.php
@@ -2,6 +2,8 @@
 
 	<div id="gohome"><a href="/"><img src="img/gohome.png"></a></div>	
 
+	<div id="news">Rencontre des pirates ce <b>vendredi 8 juin à 19h30</b> sur la place des Doyens à Louvain-La-Neuve.  <b>Feel welcome!</b></div>
+
 	<a href="/"><img src="img/Logo-bw.png" alt="logo du ppbw"/></a>
 	
 	<hgroup>
