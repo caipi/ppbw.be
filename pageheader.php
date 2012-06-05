3f251e3a1afc637536a44b2ecea58a892364ff43
Caïpi <caipi@ppbw.be>
2012-06-05 17:44:54 +0200

    la suite des demandes de nico et de mon extasiée découverte de github

--- pageheader.php
+++ pageheader.php
@@ -1,6 +1,4 @@
-<header> <!-- Defining the header section of the page with the appropriate tag -->
-
-	<div id="gohome"><a href="/"><img src="img/gohome.png"></a></div>	
+<header> <!-- Defining the header section of the page with the appropriate tag -->	
 
 	<div id="news">Rencontre des pirates ce <b>vendredi 8 juin à 19h30</b> sur la place des Doyens à Louvain-La-Neuve.  <b>Feel welcome!</b></div>
 
@@ -12,6 +10,7 @@
     
 	<nav class="clear"> <!-- The nav link semantically marks your main site navigation -->
 		<ul>
+			<li><a href="/">Accueil</a></li>
 			<li><a href="ensavoirplus.php">En savoir plus</a></li>
 			<li><a href="contact.php">Contact</a></li>
 		</ul>		
