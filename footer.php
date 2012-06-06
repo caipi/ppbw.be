14487edd7400ddaec06a0d98727549465824d81d
Caïpi <caipi@ppbw.be>
2012-06-06 02:03:58 +0200

    design by des pirates

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <footer> <!-- Marking the footer section -->
    
-   <p><?php derniereModif(); ?> - Copyleft 2012 - Ppbw.be - Design by <span class="caipi">Ca&Iuml;pi</span></p>
+   <p><?php derniereModif(); ?> - Copyleft 2012 - Ppbw.be - Design by <span class="designby">Des pirates</span></p>
    <a class="by" href="http://tutorialzine.com/2010/02/html5-css3-website-template/" >Theme inspired by Tutorialzine</a>
    <a class="up" href="#" >Go UP</a>
 
