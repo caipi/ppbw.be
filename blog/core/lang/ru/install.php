48ea05743b4678e0512350a451df41c39cc24517
Adrien le Maire <alemaire@delle.home>
2012-06-09 13:14:04 +0200

    add blog url admin: blog/core/admin user: ppbw pass: changeme

---
+++ blog/core/lang/ru/install.php
@@ -0,0 +1,34 @@
+<?php
+
+$LANG = array(
+
+# install.php
+'L_INSTALL_TITLE'					=> 'Инсталяция',
+'L_WRONG_PHP_VERSION'				=> 'PluXml требует PHP 5 и выше',
+'L_SELECT_LANG'						=> 'Выберите ваш язык',
+'L_INPUT_CHANGE'					=> 'Изменение',
+'L_DEFAULT_CATEGORY_TITLE'			=> 'Категория 1',
+'L_DEFAULT_CATEGORY_URL'			=> 'caregory-1',
+'L_DEFAULT_STATIC_TITLE'			=> 'Статическая 1',
+'L_DEFAULT_STATIC_URL'				=> 'static-1',
+'L_DEFAULT_STATIC_CONTENT'			=> 'Мои первые статические страницы !',
+'L_DEFAULT_ARTICLE_TITLE'			=> 'Первая статья',
+'L_DEFAULT_ARTICLE_URL'				=> 'first-article',
+'L_DEFAULT_ARTICLE_CONTENT'			=> 'Данная статья была создана во время установки PluXml. Изменить его в панели администрации.',
+'L_DEFAULT_COMMENT_CONTENT'			=> 'Это первый комментарий!',
+'L_ERR_PLUXML_ALREADY_INSTALLED'	=> 'PluXml уже создана !',
+'L_ERR_MISSING_USER'				=> 'Пожалуйста, заполните поля !',
+'L_ERR_MISSING_LOGIN'				=> 'Пожалуйста, заполните поля Логин !',
+'L_ERR_MISSING_PASSWORD'			=> 'Пожалуйста, заполните поле пароля!',
+'L_ERR_PASSWORD_CONFIRMATION'		=> 'Подтверждение Неверный пароль !',
+'L_PLUXML_INSTALLATION'				=> 'PluXml установка',
+'L_SITE_DESCRIPTION'				=> 'Блог или CMS !',
+'L_VERSION'							=> 'версия',
+'L_USERNAME'						=> 'Имя Администратора',
+'L_LOGIN'							=> 'Логин Администратора',
+'L_PASSWORD'						=> 'Пароль',
+'L_PASSWORD_CONFIRMATION'			=> 'Подтвердить пароль',
+'L_INPUT_INSTALL'					=> 'Установка',
+'L_TIMEZONE'						=> 'Время',
+);
+?>
---
+++ blog/themes/pirate/erreur.php
@@ -0,0 +1,18 @@
+<?php include(dirname(__FILE__).'/header.php'); ?>
+
+       <section id="articles">
+
+                <article id="blog">
+                <div class="articleBody clear">
+
+
+				<h2><?php $plxShow->lang('ERROR') ?> :</h2>
+				<div class="error-content"><?php $plxShow->erreurMessage(); ?></div>
+
+                </div>
+                </article>
+        </section>
+
+
+<?php include(dirname(__FILE__).'/footer.php'); ?>
+
